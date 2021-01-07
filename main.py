8768ed794abfb89c46860e305ced075d6a83d3b2
Lex <lex.shornikov@yandex.ru>
2021-01-07 18:11:31 +0300

    Создание холста, а так же функции для инициализации

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import sys
 
+import pygame
+
+
+class Game():
+    def __init__(self):
+        # задаем размеры экрана
+        self.screen_width = 720
+        self.screen_height = 460
+
+        # необходимые цвета
+        self.red = pygame.Color(255, 0, 0)
+        self.sweet_green = pygame.Color(154, 255, 154)
+        self.green = pygame.Color(0, 139, 0)
+        self.black = pygame.Color(0, 0, 0)
+        self.white = pygame.Color(255, 255, 255)
+        self.brown = pygame.Color(165, 42, 42)
+
+        # Frame per second controller
+        # будет задавать количество кадров в секунду
+        self.fps_controller = pygame.time.Clock()
+
+        # переменная для оторбражения результата
+        # (сколько еды съели)
+        self.score = 0
+
+    def init_and_check_for_errors(self):
+        """Начальная функция для инициализации и
+           проверки как запустится pygame"""
+        check_errors = pygame.init()
+        if check_errors[1] > 0:
+            sys.exit()
+        else:
+            print('ok')
