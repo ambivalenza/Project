cbb9ee6e3d67785742e36a08dfc314b5935d110d
polnet <paul.bor.0808@gmail.com>
2021-01-08 15:21:58 +0300

    Added function set_surface_and_title, event_loop, refresh_screen, start_screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
-
+import random
+import time
+import os
 import pygame
 
 
@@ -33,3 +35,63 @@ class Game():
             sys.exit()
         else:
             print('Bruh')
+
+    def set_surface_and_title(self):
+        """Задаем surface(поверхность поверх которой будет все рисоваться)
+        и устанавливаем загаловок окна"""
+        self.play_surface = pygame.display.set_mode((
+            self.screen_width, self.screen_height))
+        pygame.display.set_caption('Snake Game')
+
+    def event_loop(self, change_to):
+        """Функция для отслеживания нажатий клавиш игроком"""
+
+        # запускаем цикл по ивентам
+        for event in pygame.event.get():
+            # если нажали клавишу
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                    change_to = "RIGHT"
+                elif event.key == pygame.K_LEFT or event.key == ord('a'):
+                    change_to = "LEFT"
+                elif event.key == pygame.K_UP or event.key == ord('w'):
+                    change_to = "UP"
+                elif event.key == pygame.K_DOWN or event.key == ord('s'):
+                    change_to = "DOWN"
+                # нажали escape
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        return change_to
+
+    def refresh_screen(self):
+        """обновляем экран и задаем фпс"""
+        pygame.display.flip()
+        game.fps_controller.tick(20)
+
+    def start_screen(self):
+        intro_text = ["ЗМЕЙКА", "",
+                      "В разработке принемали участие", ""
+                      "Боровков Павел и Шорников Алексея", "", "", "", "",
+                      "НАЖМИТЕ ЧТОБЫ НАЧАТЬ"]
+        name = os.path.join('D:\PycharmProjects\Data\zmeya-zelenyy-fon-minimalizm.jpg')
+        load_image = pygame.image.load(name)
+        fon = pygame.transform.scale(load_image, (self.screen_width, self.screen_height))
+        self.play_surface.blit(fon, (0, 0))
+        font = pygame.font.SysFont('monaco', 25)
+        text_coord = 30
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color(19, 137, 8))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 50
+            text_coord += intro_rect.height
+            self.play_surface.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return  # начинаем игру
+            pygame.display.flip()
